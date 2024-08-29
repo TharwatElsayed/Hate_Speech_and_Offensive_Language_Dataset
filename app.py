776337e1f99ec1234beee40b4fb6eac12f5a1a12
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2024-08-30 01:15:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ df = pd.read_csv('labeled_data.csv')
 # Set Streamlit page title
 st.title('Hate Speech and Offensive Language Analysis')
 
+# Horizontal line separator
+st.markdown("---")
+
 # Display basic dataset information
 st.subheader('Dataset Pefore Preprocessing Overview')
 st.write(df.describe())
