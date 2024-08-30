3e2914838faf8efd6752eb07e771f2dd8c18b026
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2024-08-30 17:37:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,37 +22,6 @@ st.write(df.describe(include='all'))
 # Horizontal line separator
 st.markdown("---")
 
-# Count the occurrences of each class
-# Bar chart
-st.subheader('Distribution of Classes (Bar Chart)')
-#Assuming 'class' is the name of the column you want to create a histogram for
-# Create the histogram
-plt.figure(figsize=(8, 6))
-plt.hist(df['class'], bins=3, edgecolor='black')
-plt.xlabel('Class')
-plt.ylabel('Frequency')
-plt.title('Distribution of Classes')
-plt.xticks([0, 1, 2], ['Hate Speech', 'Offensive Language', 'Neither'])
-# Display the plot in Streamlit
-st.pyplot(plt)
-
-# Pie chart
-st.subheader('Proportion of Classes (Pie Chart)')
-labels = 'Hate Speech', 'Offensive Language', 'Neither'
-sizes = df['class'].value_counts()
-explode = (0, 0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-plt.title('Distribution of Classes')
-# Display the plot in Streamlit
-st.pyplot(plt)
-
-# Horizontal line separator
-st.markdown("---")
-
 # Set Streamlit page title
 st.title('Hate Speech and Offensive Language Analysis')
 
